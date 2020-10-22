a6eee3aa8d7ce72a25426cf4db9644b8f90737c4
lianxue <lianxueko@qq.com>
2020-10-22 17:28:12 +0800

    commit config

--- logs/log.js
+++ logs/log.js
@@ -2,7 +2,7 @@ var log4js = require('log4js');
 log4js.configure({
     pm2: true,
     appenders: { middleware: { type: 'file', filename: 'logs/middleware.log' } },
-    categories: { default: { appenders: ['middleware'], level: 'error' } }
+    categories: { default: { appenders: ['middleware'], level: 'all' } }
 });
 var logger = log4js.getLogger('middleware');
 module.exports = logger;
