dbb1199818fb8af1614ca4d94740984954fe0ec4
lianxue <lianxueko@qq.com>
2020-11-05 11:18:34 +0800

    post

--- server/http.js
+++ server/http.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const qs = require('qs');
 const log = require('./../logs/log.js')
 const service = axios.create({
     baseURL: "", // api的base_url  process.env.BASE_API,,注意局域网访问时，不能使用localhost
@@ -6,12 +7,20 @@ const service = axios.create({
 })
 
 service.interceptors.request.use(config => {
-    console.log('global.Token',global.Token);
-    console.log('config',config);
-    config.headers['Auth-Token'] = global.Token;
-    console.log('当前请求的url',config.url)
-    log.info('当前请求的url',config.url)
-    
+    console.log('global.Token', global.Token);
+    console.log('config', config);
+    console.log('当前请求的url', config.url)
+    log.info('当前请求的url', config.url)
+
+    if (global.Token) {
+        config.headers['Auth-Token'] = global.Token
+    }
+    // get 请求数组处理
+    if (config.method === 'get') {
+        config.paramsSerializer = function (params) {
+            return qs.stringify(params, { arrayFormat: 'repeat' })
+        }
+    }
     return config
 }, error => {
     console.log('error:', error) // for debug
--- server/routes/teamweb/index.js
+++ server/routes/teamweb/index.js
@@ -13,5 +13,22 @@ router.get('/getListNew', function (req, res, next) {
             res.json({ error: error.code, message: error.message });
         });
 });
+router.post('/posttest', function (req, res, next) {
+    console.log('posttest req body', JSON.stringify(req.body))
+    log.info('posttest req body', req.body)
+    http.service({
+        url: 'http://sy.smartlink-tech.com.cn:81/smartteam/test/truck/mil/report/export',
+        method: 'post',
+        data: req.body
+    }
+    ).then((response) => {
+        res.json(response.data)
+    })
+        .catch((error) => {
+            console.log('posttest err', error)
+            log.error('posttest err', error);
+            res.json({ error: error.code, message: error.message });
+        });
+});
 
 module.exports = router;
